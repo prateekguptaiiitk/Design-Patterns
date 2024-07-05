00e3dfbb35416e50829dcf76df7ee90215506a03
Prateek Gupta <prateekjpg@gmail.com>
2024-07-05 16:03:33 +0530

    Add complete code for tictactoe game

--- tictactoe.py
+++ tictactoe.py
@@ -16,7 +16,7 @@ class PlayingPieceX(PlayingPiece):
 
 class PlayingPieceO(PlayingPiece):
 	def __init__(self):
-		super().__init__(PieceType.Y)
+		super().__init__(PieceType.O)
 
 class Board():
 	size = 0
@@ -24,24 +24,36 @@ class Board():
 
 	def __init__(self, size):
 		self.size = size
-		self.board = [[None] * (size)] * (size)
+		self.board = [[None for _ in range(self.size)] for _ in range(self.size)]
 
 	def addPiece(self, row, col, playingPiece):
-		if board[row][col]:
+		if self.board[row][col]:
 			return False
 
-		board[row][col] = playingPiece
+		self.board[row][col] = playingPiece
 		return True
 
 	def getFreeCells(self):
 		freeCells = []
 
-		for i in range(size):
-			for j in range(size):
-				if not board[i][j]:
-					freeCells.add((i, j))
+		for i in range(self.size):
+			for j in range(self.size):
+				if not self.board[i][j]:
+					freeCells.append((i, j))
 
 		return freeCells
+
+	def printBoard(self):
+		for i in range(self.size):
+			for j in range(self.size):
+				if self.board[i][j] != None:
+					print(self.board[i][j].pieceType.name + ' ', end='')
+				else:
+					print('  ', end='')
+
+				print(' | ', end='')
+
+			print()
 
 class Player():
 	_name = None
@@ -91,12 +103,58 @@ class TicTacToeGame():
 			playerTurn = self.players.popleft()
 
 			# get the free space from the board
-			gameBoard.printBoard()
-			freeSpaces = gameBoard.getFreeCells()
+			self.gameBoard.printBoard()
+			freeSpaces = self.gameBoard.getFreeCells()
 			if len(freeSpaces) == 0:
 				noWinner = False
 				continue
 
 			# read the user input
-			s = input('Player:', playerTurn._name, ' Enter row, column: ')
-			
+			inpRow, inpCol = map(int, input('Player: ' + playerTurn._name + ' enter row, column: ').split(','))
+
+			pieceAddedSuccessfully = self.gameBoard.addPiece(inpRow, inpCol, playerTurn.getPlayingPiece())
+			if not pieceAddedSuccessfully:
+				print('Incorrect position choosen, try again')
+				self.players.appendleft(playerTurn)
+				continue
+
+			self.players.append(playerTurn)
+
+			winner = self.isThereWinner(inpRow, inpCol, playerTurn.getPlayingPiece().pieceType)
+			if winner:
+				return playerTurn.getName()
+
+
+		return 'Tie'
+
+	def isThereWinner(self, row, col, pieceType):
+		rowMatch = True
+		colMatch = True
+		diagonalMatch = True
+		antiDiagonalMatch = True
+
+		# need to check in row
+		for i in range(len(self.gameBoard.board)):
+			if self.gameBoard.board[row][i] == None or self.gameBoard.board[row][i].pieceType != pieceType:
+				rowMatch = False
+
+		# need to check in column
+		for i in range(len(self.gameBoard.board)):
+			if self.gameBoard.board[i][col] == None or self.gameBoard.board[i][col].pieceType != pieceType:
+				colMatch = False
+
+		# need to check diagonals
+		for i in range(len(self.gameBoard.board)):
+			if self.gameBoard.board[i][i] == None or self.gameBoard.board[i][i].pieceType != pieceType:
+				diagonalMatch = False
+
+		# need to check anti-diagonals
+		for i in range(len(self.gameBoard.board)):
+			if self.gameBoard.board[i][len(self.gameBoard.board)-1 - i] == None or self.gameBoard.board[i][len(self.gameBoard.board)-1-i].pieceType != pieceType:
+				antiDiagonalMatch = False
+
+		return rowMatch or colMatch or diagonalMatch or antiDiagonalMatch
+
+if __name__ == '__main__':
+	game = TicTacToeGame()
+	print('game winner is: ', game.startGame())
