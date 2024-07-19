92aaf3d101125121d672cc92c56d52a287ef3ea7
Prateek Gupta <prateekjpg@gmail.com>
2024-07-19 12:42:28 +0530

    Add file system code

---
+++ filesystem.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+class FileSystem(ABC):
+    @abstractmethod
+    def ls(self):
+        pass
+
+class File(FileSystem):
+    fileName = None
+
+    def __init__(self, name):
+        self.fileName = name
+
+    def ls(self):
+        print("file name ", self.fileName)
+
+class Directory(FileSystem):
+    directoryName = None
+    fileSystemList = None
+
+    def __init__(self, name):
+        self.directoryName = name
+        self.fileSystemList = []
+
+    def add(self, fileSystemObj):
+        self.fileSystemList.append(fileSystemObj)
+
+    def ls(self):
+        print("Directory name ", self.directoryName)
+
+        for fileSystemObj in self.fileSystemList:
+            fileSystemObj.ls()
+
+if __name__ == '__main__':
+    movieDirectory = Directory("Movie")
+    border = File("Border")
+    movieDirectory.add(border)
+
+    comedyMovieDirectory = Directory("ComedyMovie")
+    hulchul = File("Hulchul")
+    comedyMovieDirectory.add(hulchul)
+    movieDirectory.add(comedyMovieDirectory)
+
+    movieDirectory.ls()
+
