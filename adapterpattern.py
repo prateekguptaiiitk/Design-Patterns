9c2e7c8df33ebfa98625c195c1777e621f54784a
Prateek Gupta <prateekjpg@gmail.com>
2024-07-19 16:05:38 +0530

    Add adapter design pattern code

---
+++ adapterpattern.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+
+# adaptee
+class WeightMaching(ABC):
+    
+    @abstractmethod
+    def getWeightInPound(self):
+        pass
+
+class WeightMachineForBabies(WeightMaching):
+    def getWeightInPound(self):
+        weight = int(input('Enter weight in pounds: '))
+        return weight
+    
+# adapter
+class WeightMachineAdapter(ABC):
+    
+    @abstractmethod
+    def getWeightInKg(self):
+        pass
+
+class WeightMachineAdapterImpl(WeightMachineAdapter):
+
+    weightMachine = None
+
+    def __init__(self, weightMachine):
+        self.weightMachine = weightMachine
+    
+    def getWeightInKg(self):
+        weightInPound = self.weightMachine.getWeightInPound()
+
+        # Convert it to KGs
+        weightInKg = weightInPound * 0.45
+        return weightInKg
+
+# client
+if __name__ == '__main__':
+    weightMachineAdapter = WeightMachineAdapterImpl(WeightMachineForBabies())
+    print(weightMachineAdapter.getWeightInKg())
