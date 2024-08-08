610664fa2c377b39d5905077fbee49ff14c76648
Prateek Gupta <prateekjpg@gmail.com>
2024-08-08 16:30:00 +0530

    Add code changes to decorator pattern

--- decoratorpattern.py
+++ decoratorpattern.py
@@ -1,59 +1,51 @@
 from abc import ABC, abstractmethod
 
 class BasePizza(ABC):
-
 	@abstractmethod
 	def cost(self):
 		pass
 
 class Farmhouse(BasePizza):
-
 	def cost(self):
 		return 200
 
 class VegDelight(BasePizza):
-
 	def cost(self):
 		return 120
 
 class Margheretta(BasePizza):
-
 	def cost(self):
 		return 100
 
 class ToppingDecorator(BasePizza):
-
-	_basePizza = None
+	base_pizza = None
 	
-	def __init__(self, basePizza):
-		self._basePizza = basePizza
+	def __init__(self, base_pizza):
+		self.base_pizza = base_pizza
 
-	def getBasePizzaCost(self):
-		return self._basePizza.cost()
+	def get_base_pizza_cost(self):
+		return self.base_pizza.cost()
 
 	def cost(self):
 		pass
 
-
 class ExtraCheese(ToppingDecorator):
-
-	def __init__(self, basePizza):
-		super().__init__(basePizza)
+	def __init__(self, base_pizza):
+		super().__init__(base_pizza)
 	
 	def cost(self):
-		return self.getBasePizzaCost() + 10
-
+		return self.get_base_pizza_cost() + 10
 
 class Mushrooms(ToppingDecorator):
-
-	def __init__(self, basePizza):
-		super().__init__(basePizza)
+	def __init__(self, base_pizza):
+		super().__init__(base_pizza)
 	
 	def cost(self):
-		return self.getBasePizzaCost() + 15
+		return self.get_base_pizza_cost() + 15
 
-BasePizza = ExtraCheese(Margheretta())
-print(BasePizza.cost())
+if __name__ == '__main__':
+	pizza = ExtraCheese(Margheretta())
+	print(pizza.cost())
 
-BasePizza = Mushrooms(ExtraCheese(Margheretta()))
-print(BasePizza.cost())
+	pizza = Mushrooms(ExtraCheese(Margheretta()))
+	print(pizza.cost())
