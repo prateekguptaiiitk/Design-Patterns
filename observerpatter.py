fbc8a2418a49ab8402d503aa3a7d5bc410d11330
Prateek Gupta <prateekjpg@gmail.com>
2024-08-08 16:36:23 +0530

    Add minor code changes to observer pattern

--- observerpatter.py
+++ observerpatter.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 
 class StockObservable(ABC):
-
 	@abstractmethod
 	def add(self, observer):
 		pass
@@ -12,91 +11,87 @@ class StockObservable(ABC):
 		pass
 
 	@abstractmethod
-	def notifySubscriber(self):
+	def notify_subscriber(self):
 		pass
 
 	@abstractmethod
-	def setStockCount(self, newStockAdded):
+	def set_stock_count(self, newStockAdded):
 		pass
 
 	@abstractmethod
-	def getStockCount(self):
+	def get_stock_count(self):
 		pass
 
 class IphoneObservable(StockObservable):
-
-	_stockCount = 0
-	_observers = []
+	stock_count = 0
+	observers = []
 
 	def add(self, observer):
-		self._observers.append(observer)
+		self.observers.append(observer)
 
 	def remove(self, observer):
-		self._observers.remove(observer)
+		self.observers.remove(observer)
 
-	def notifySubscriber(self):
-		for observer in self._observers:
+	def notify_subscriber(self):
+		for observer in self.observers:
 			observer.update()
 
-	def setStockCount(self, newStockAdded):
-		if self._stockCount == 0:
-			self.notifySubscriber()
+	def set_stock_count(self, new_stock_added):
+		if self.stock_count == 0:
+			self.notify_subscriber()
 
-		self._stockCount += newStockAdded
+		self.stock_count += new_stock_added
 
-	def getStockCount(self):
-		return self._stockCount
-
+	def get_stock_count(self):
+		return self.stock_count
 
 class NotificationAlertObserver(ABC):
-
 	@abstractmethod
 	def update(self):
 		pass
-
 
 class EmailAlertObserver(NotificationAlertObserver):
 	_emailId = ''
 	_observable = None
 
-	def __init__(self, emailId, observable):
+	def __init__(self, email_id, observable):
 		self._observable = observable
-		self._emailId = emailId
+		self.email_id = email_id
 
 	def update(self):
-		self.sendMessage(self._emailId, 'product is back in stock')
+		self.send_message(self.email_id, 'product is back in stock')
 
-	def sendMessage(self, emailId, msg):
-		print('email sent to:', emailId)
+	def send_message(self, email_id, msg):
+		print('email sent to:', email_id)
 
 class MobileAlertObserver(NotificationAlertObserver):
-	_mobileNo = ''
-	_observable = None
+	mobile_no = ''
+	observable = None
 
-	def __init__(self, mobileNo, observable):
-		self._observable = observable
-		self._mobileNo = mobileNo
+	def __init__(self, mobile_no, observable):
+		self.observable = observable
+		self.mobile_no = mobile_no
 
 	def update(self):
-		self.sendMessage(self._mobileNo, 'product is back in stock')
+		self.send_message(self.mobile_no, 'product is back in stock')
 
-	def sendMessage(self, mobileNo, msg):
-		print('msg sent to mobile no.:', mobileNo)
+	def send_message(self, mobile_no, msg):
+		print('msg sent to mobile no.:', mobile_no)
 
 if __name__ == '__main__':
-	iphoneStockObservable = IphoneObservable()
-	observer1 = EmailAlertObserver('abc@gmail.com', iphoneStockObservable)
-	observer2 = EmailAlertObserver('xyz@gmail.com', iphoneStockObservable)
-	observer3 = MobileAlertObserver('1122334455', iphoneStockObservable)
+	iphone_stock_observable = IphoneObservable()
+	observer1 = EmailAlertObserver('abc@gmail.com', iphone_stock_observable)
+	observer2 = EmailAlertObserver('xyz@gmail.com', iphone_stock_observable)
+	observer3 = MobileAlertObserver('1122334455', iphone_stock_observable)
 
-	iphoneStockObservable.add(observer1)
-	iphoneStockObservable.add(observer2)
-	iphoneStockObservable.add(observer3)
+	iphone_stock_observable.add(observer1)
+	iphone_stock_observable.add(observer2)
+	iphone_stock_observable.add(observer3)
 
-	iphoneStockObservable.setStockCount(10)
-	iphoneStockObservable.setStockCount(-10)
+	iphone_stock_observable.set_stock_count(10)
+	iphone_stock_observable.set_stock_count(-10)
 
-	iphoneStockObservable.remove(observer2)
+	iphone_stock_observable.remove(observer2)
 
-	iphoneStockObservable.setStockCount(40)
-	print('Current stock count:', iphoneStockObservable.getStockCount())
+	iphone_stock_observable.set_stock_count(40)
+	print('Current stock count:', iphone_stock_observable.get_stock_count())
