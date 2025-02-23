6f5d44a61a77f70973537c6a7df4313bacb81548
Prateek Gupta <prateekjpg@gmail.com>
2025-02-23 14:51:39 +0530

    Remove redundant code from prototype pattern

--- prototypepattern.py
+++ prototypepattern.py
@@ -10,16 +10,12 @@ class Student(Prototype):
         self.age = age
         self.roll_number = roll_number
         self.name = name
-
-    @classmethod
-    def create_student(cls, age, roll_number, name):
-        return cls(age, roll_number, name)
-
+        
     def clone(self):
         return Student(self.age, self.roll_number, self.name)
 
 if __name__ == '__main__':
-    obj = Student.create_student(20, 75, 'John')
+    obj = Student(20, 75, 'John')
     clone_obj = obj.clone()
 
     print(f'Original: age={obj.age}, roll_number={obj.roll_number}, name={obj.name}')
