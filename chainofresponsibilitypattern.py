915fd5d88b1009fcdb6489d79d0cdefc729427ff
Prateek Gupta <prateekjpg@gmail.com>
2025-02-23 21:06:10 +0530

    Minor change

--- chainofresponsibilitypattern.py
+++ chainofresponsibilitypattern.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-class LogType(Enum):
+class LogLevel(Enum):
     INFO = 1
     DEBUG = 2
     ERROR = 3
@@ -18,7 +18,7 @@ class InfoLogProcessor(LogProcessor):
         super().__init__(next_logger_processor)
 
     def log(self, log_level, message):
-        if log_level == LogType.INFO:
+        if log_level == LogLevel.INFO:
             print('INFO: ', message)
         else:
             super().log(log_level, message)
@@ -28,7 +28,7 @@ class DebugLogProcessor(LogProcessor):
         super().__init__(next_logger_processor)
 
     def log(self, log_level, message):
-        if log_level == LogType.DEBUG:
+        if log_level == LogLevel.DEBUG:
             print('DEBUG: ', message)
         else:
             super().log(log_level, message)
@@ -38,7 +38,7 @@ class ErrorLogProcessor(LogProcessor):
         super().__init__(next_logger_processor)
 
     def log(self, log_level, message):
-        if log_level == LogType.ERROR:
+        if log_level == LogLevel.ERROR:
             print('ERROR: ', message)
         else:
             super().log(log_level, message)
@@ -46,6 +46,6 @@ class ErrorLogProcessor(LogProcessor):
 if __name__ == '__main__':
     logObject = InfoLogProcessor(DebugLogProcessor(ErrorLogProcessor(None)))
 
-    logObject.log(LogType.ERROR, "exception happens")
-    logObject.log(LogType.DEBUG, "need to debug this ")
-    logObject.log(LogType.INFO, "just for info ")
+    logObject.log(LogLevel.ERROR, "exception happens")
+    logObject.log(LogLevel.DEBUG, "need to debug this ")
+    logObject.log(LogLevel.INFO, "just for info ")
