6339e63ce394f850143d2eca51d5e0b5837f1576
Prateek Gupta <prateekjpg@gmail.com>
2026-01-18 17:45:55 +0530

    Update flyweightpattern.py

--- flyweightpattern/flyweightpattern.py
+++ flyweightpattern/flyweightpattern.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from collections import defaultdict
 
 class ILetter(ABC):
     @abstractmethod
@@ -12,26 +13,25 @@ class DocumentCharacter(ILetter):
         self.size = size
     
     def get_type(self):
-        return self.type
+        return self.font_type
     
     def get_body(self):
-        return self.body
+        return self.character
 
     def display(self, row, col):
-        # use character of a particular font type and size at a given location
-        print('Character:', self.character, 'font:', self.font_type, 'Size:', self.size, 'At position (x, y):', row, col)
+        print('Character:', self.character, 'font:', self.font_type, 
+              'Size:', self.size, 'At position (x, y):', row, col)
 
-from collections import defaultdict
 class LetterFactory:
     character_cache = defaultdict(ILetter)
 
     @classmethod
-    def create_letter(self, character_value):
-        if character_value in self.character_cache:
-            return self.character_cache[character_value]
+    def create_letter(cls, character_value):  # Use 'cls' convention
+        if character_value in cls.character_cache:
+            return cls.character_cache[character_value]
         
-        character_obj = DocumentCharacter(character_value, 'Ariel', 10)
-        self.character_cache[character_value] = character_obj
+        character_obj = DocumentCharacter(character_value, 'Arial', 10)
+        cls.character_cache[character_value] = character_obj
         return character_obj
 
 if __name__ == '__main__':
@@ -43,8 +43,9 @@ if __name__ == '__main__':
         h = 3 times
         a = 3 times and so on...
     '''
+    
     obj1 = LetterFactory.create_letter('t')
-    obj1.display(0, 0)
+    obj1.display(0, 0)  # Character: t font: Arial Size: 10 At position (x, y): 0 0
 
-    obj2 = LetterFactory.create_letter('t')
-    obj2.display(0, 6)
+    obj2 = LetterFactory.create_letter('t')  # Returns cached object
+    obj2.display(0, 6)  # Same object, different position
