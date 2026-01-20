62dfe0d482bea81a61d1617de19db1477dec0bff
Prateek Gupta <prateekjpg@gmail.com>
2026-01-20 11:12:15 +0530

    Remove type hints from drive method and constructors

--- strategypattern/strategypattern.py
+++ strategypattern/strategypattern.py
@@ -1,32 +1,31 @@
-from __future__ import annotations
 from abc import ABC, abstractmethod
 
 class DriveStrategy(ABC):
     @abstractmethod
-    def drive(self) -> None:
+    def drive(self):
         pass
 
 class NormalDriveStrategy(DriveStrategy):
-    def drive(self) -> None:
+    def drive(self):
         print('Normal Drive Strategy')
 
 class SportsDriveStrategy(DriveStrategy):
-    def drive(self) -> None:
+    def drive(self):
         print('Sports Drive Strategy')
 
 class Vehicle:
-    def __init__(self, strategy: DriveStrategy) -> None:
+    def __init__(self, strategy):
         self.strategy = strategy
 
-    def drive(self) -> None:
+    def drive(self):
         self.strategy.drive()
 
 class OffRoadVehicle(Vehicle):
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__(SportsDriveStrategy())
 
 class PassengerVehicle(Vehicle):
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__(NormalDriveStrategy())
 
 # Testing the implementation
