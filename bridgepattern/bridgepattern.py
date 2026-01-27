06e43731dbd42401f167f4ab7f24d62515353805
Prateek Gupta <prateekjpg@gmail.com>
2026-01-27 15:25:51 +0530

    Refactor Dog, Fish, and Tree classes
    Removed unnecessary constructors from Dog, Fish, and Tree classes.

--- bridgepattern/bridgepattern.py
+++ bridgepattern/bridgepattern.py
@@ -8,24 +8,15 @@ class LivingThings(ABC):
     def breathe_process(self):
         pass
 
-class Dog(LivingThings):
-    def __init__(self, breathe_implementor):
-        super().__init__(breathe_implementor)
-    
+class Dog(LivingThings):    
     def breathe_process(self):
         self.breathe_implementor.breathe()
 
-class Fish(LivingThings):
-    def __init__(self, breathe_implementor):
-        super().__init__(breathe_implementor)
-    
+class Fish(LivingThings):    
     def breathe_process(self):
         self.breathe_implementor.breathe()
 
-class Tree(LivingThings):
-    def __init__(self, breathe_implementor):
-        super().__init__(breathe_implementor)
-    
+class Tree(LivingThings):    
     def breathe_process(self):
         self.breathe_implementor.breathe()
 
@@ -37,20 +28,14 @@ class BreatheImplementor(ABC):
 class LandBreatheImplementation(BreatheImplementor):
     def breathe(self):
         print('breathes through nose')
-        print('inhale oxygen from air')
-        print('exhale carbon dioxide')
 
 class WaterBreatheImplementation(BreatheImplementor):
     def breathe(self):
         print('breathes through gills')
-        print('inhale oxygen from water')
-        print('exhale carbon dioxide')
 
 class TreeBreatheImplementation(BreatheImplementor):
     def breathe(self):
         print('breathes through leaves')
-        print('inhale carbon dioxide')
-        print('exhale oxygen through photosynthesis')
 
 if __name__ == '__main__':
     fish_obj = Fish(WaterBreatheImplementation())
