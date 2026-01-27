313e2b55249235f0325fc22d4d6c03257bdba89e
Prateek Gupta <prateekjpg@gmail.com>
2026-01-27 16:19:37 +0530

    Refactor ToppingDecorator and cost methods

--- decoratorpattern/decoratorpattern.py
+++ decoratorpattern/decoratorpattern.py
@@ -1,51 +1,47 @@
 from abc import ABC, abstractmethod
 
 class BasePizza(ABC):
-	@abstractmethod
-	def cost(self):
-		pass
+    @abstractmethod
+    def cost(self):
+        pass
 
 class Farmhouse(BasePizza):
-	def cost(self):
-		return 200
+    def cost(self):
+        return 200
 
 class VegDelight(BasePizza):
-	def cost(self):
-		return 120
+    def cost(self):
+        return 120
 
 class Margheretta(BasePizza):
-	def cost(self):
-		return 100
+    def cost(self):
+        return 100
 
-class ToppingDecorator(BasePizza):
-	base_pizza = None
-	
-	def __init__(self, base_pizza):
-		self.base_pizza = base_pizza
+class ToppingDecorator(BasePizza, ABC):    
+    def __init__(self, base_pizza):
+        self.base_pizza = base_pizza
 
-	def get_base_pizza_cost(self):
-		return self.base_pizza.cost()
-
-	def cost(self):
-		pass
+    @abstractmethod
+    def cost(self):
+        pass
 
 class ExtraCheese(ToppingDecorator):
-	def __init__(self, base_pizza):
-		super().__init__(base_pizza)
-	
-	def cost(self):
-		return self.get_base_pizza_cost() + 10
+    def __init__(self, base_pizza):
+        super().__init__(base_pizza)
+    
+    def cost(self):
+        return self.base_pizza.cost() + 10
 
 class Mushrooms(ToppingDecorator):
-	def __init__(self, base_pizza):
-		super().__init__(base_pizza)
-	
-	def cost(self):
-		return self.get_base_pizza_cost() + 15
+    def __init__(self, base_pizza):
+        super().__init__(base_pizza)
+    
+    def cost(self):
+        return self.base_pizza.cost() + 15
 
 if __name__ == '__main__':
-	pizza = ExtraCheese(Margheretta())
-	print(pizza.cost())
+    pizza = ExtraCheese(Margheretta())
+    print(pizza.cost())
 
-	pizza = Mushrooms(ExtraCheese(Margheretta()))
-	print(pizza.cost())
+    pizza = Mushrooms(ExtraCheese(Margheretta()))
+    print(pizza.cost())
